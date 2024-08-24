25554760fc8206b5a6a2d76e929556d7a3aceedb
parth799 <118920131+parth799@users.noreply.github.com>
2024-08-24 10:44:01 +0530

    feat: Initial setup with Vite, React, Redux, and Tailwind CSS

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import App from './App.jsx'
+import './index.css'
+import {Provider} from "react-redux"
+import store from './store/store.js'
+import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+)
---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const store  = configureStore({
+    reducer: {
+        // Define your reducers here
+    }
+})
+
+export default store;
