1091a8212a500a40ac0679021360c34458091fa4
parth799 <118920131+parth799@users.noreply.github.com>
2024-08-26 10:54:50 +0530

    feat: Add Button component and update CSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.js
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ], 
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
 
